6f57e82e95be4d63b51369eea53bd866b5c73734
Roman Vakulchik <roman.vakulchik@gmail.com>
2014-10-27 22:36:24 +0300

    Refactoring code

---
+++ bash.org/README.md
@@ -0,0 +1,2 @@
+Utility in the file bash.rb allow you parse quotes on http://bash.im in Xml format.
+P.S.: Waiting while program execute before minute. 
--- bash.org/bash.rb
+++ bash.org/bash.rb
@@ -4,20 +4,18 @@ require 'rubygems'
 require 'mechanize'
 
 agent = Mechanize.new
-
 quotes = []
-agent.get('http://bash.im') do |page|
-  page.links_with(:text => /#/).each do |link|
-    #link.click
 
+agent.get('http://bash.im') do |page|
+  page.links_with(:text => /#/).each_with_index do |link, index|
+    link.click
     name = link.text
-    rating = agent.get(link.click).search("[@class='rating-o']").children.text
+    rating = agent.get(link).search("[@class='rating-o']").children.text
     date = agent.get(link).search("[@class='date']").children.text
     text = agent.get(link).search("[@class='text']").children.text
-    p name
+
     quote = { :name => name, :rating => rating, :date => date, :text => text }
     quotes << quote
-    #p quotes
   end
 end
 
@@ -35,7 +33,6 @@ builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
     }
   }
 end
-puts builder.to_xml
 
 File.open("bash.xml", "w+") do |file|
   file.write(builder.to_xml)
