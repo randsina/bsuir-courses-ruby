df34eecdfe80129cd0d45e18834470ceefcb91f7
Roman Vakulchik <roman.vakulchik@gmail.com>
2014-10-29 00:35:38 +0300

    Improve perfomance

--- bash.org/README.md
+++ bash.org/README.md
@@ -1,2 +1,1 @@
 Utility in the file bash.rb allow you parse quotes on http://bash.im in Xml format.
-P.S.: Waiting while program execute before minute. 
--- bash.org/bash.rb
+++ bash.org/bash.rb
@@ -2,21 +2,27 @@
 
 require 'rubygems'
 require 'mechanize'
+require 'pry'
 
 agent = Mechanize.new
 quotes = []
+reg = Regexp.new('0-9')
+site = 'http://bash.im/index/'
+next_page = ''
 
-agent.get('http://bash.im') do |page|
-  page.links_with(:text => /#/).each_with_index do |link, index|
-    link.click
-    name = link.text
-    rating = agent.get(link).search("[@class='rating-o']").children.text
-    date = agent.get(link).search("[@class='date']").children.text
-    text = agent.get(link).search("[@class='text']").children.text
+agent.get(site + next_page) do |page|
+  #current_page = page.parser.css('[class="page"]').attr('value').text.to_i
+  #pp current_page
+  page.parser.css('div.quote').each do |quote|
+    name = quote.search("[class='id']").children.text
+    rating = quote.search("[class='rating-o']").children.text
+    date = quote.search("[class='date']").children.text
+    text = quote.search("[class='text']").children.text
 
-    quote = { :name => name, :rating => rating, :date => date, :text => text }
-    quotes << quote
+    quotes << { :name => name, :rating => rating, :date => date, :text => text } unless name.empty?
   end
+  #next_page = current_page - 1
+  #pp next_page
 end
 
 builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
