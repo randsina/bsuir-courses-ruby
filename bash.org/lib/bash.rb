b2383ab51acfe6ebde5ac2e7fb829205818aa09a
Roman Vakulchik <roman.vakulchik@gmail.com>
2014-10-29 23:15:31 +0300

    Split method run

--- bash.org/lib/bash.rb
+++ bash.org/lib/bash.rb
@@ -8,19 +8,13 @@ class Bash
 
   def run
     next_page = ''
+
     while (@count_pages > 0) do
       @agent.get("#{@site}#{next_page}") do |page|
         current_page = page.parser.css('[class="page"]').attr('value').text.to_i
-        page.parser.css('div.quote').each do |quote|
-          name = quote.search("[class='id']").children.text
-          rating = quote.search("[class='rating-o']").children.text
-          date = quote.search("[class='date']").children.text
-          text = quote.search("[class='text']").children.text
-
-          @quotes << {name: name, rating: rating, date: date, text: text} unless name.empty?
-        end
+        get_quote(page)
         next_page = current_page.pred
-        @count_pages = @count_pages.pred
+        @count_pages -= 1
       end
     end
     create_xml_file(build_xml.to_xml)
@@ -28,16 +22,29 @@ class Bash
 
   private
 
+  def get_quote(page)
+    params = { name: 'id', rating: 'rating-o', date: 'date', text: 'text' }
+    page.parser.css('div.quote').each do |quote|
+      q = {}
+      params.each_pair { |key, value| q[key] = search_by_class(quote, value)}
+      @quotes << q unless q[:name].empty?
+    end
+  end
+
+  def search_by_class(quote, quote_class)
+    quote.search("[class='#{quote_class}']").children.text
+  end
+
   def build_xml
     builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
       xml.root {
         xml.Quotes {
           @quotes.each do |quote|
             xml.Quote {
-              xml.Name quote[:name]
-              xml.Rating quote[:rating]
-              xml.Date quote[:date]
-              xml.Text quote[:text]
+              xml.Name(quote[:name])
+              xml.Rating(quote[:rating])
+              xml.Date(quote[:date])
+              xml.Text(quote[:text])
             }
           end
         }
