085522a5482663e0ad80fca66c8ca2cf75172565
Roman Vakulchik <roman.vakulchik@gmail.com>
2014-10-29 23:35:48 +0300

    Refactoring code by gem 'Rubocop'

--- bash.org/lib/bash.rb
+++ bash.org/lib/bash.rb
@@ -1,3 +1,4 @@
+# Parser of site 'http://bash.im' in XML
 class Bash
   def initialize(site, count_pages)
     @agent = Mechanize.new
@@ -9,7 +10,7 @@ class Bash
   def run
     next_page = ''
 
-    while (@count_pages > 0) do
+    while @count_pages > 0
       @agent.get("#{@site}#{next_page}") do |page|
         current_page = page.parser.css('[class="page"]').attr('value').text.to_i
         get_quote(page)
@@ -26,34 +27,30 @@ class Bash
     params = { name: 'id', rating: 'rating-o', date: 'date', text: 'text' }
     page.parser.css('div.quote').each do |quote|
       q = {}
-      params.each_pair { |key, value| q[key] = search_by_class(quote, value)}
+      params.each_pair { |key, value| q[key] = quote.search("[class='#{value}']").children.text }
       @quotes << q unless q[:name].empty?
     end
   end
 
-  def search_by_class(quote, quote_class)
-    quote.search("[class='#{quote_class}']").children.text
-  end
-
   def build_xml
-    builder = Nokogiri::XML::Builder.new(:encoding => 'UTF-8') do |xml|
-      xml.root {
-        xml.Quotes {
+    builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+      xml.root do
+        xml.Quotes do
           @quotes.each do |quote|
-            xml.Quote {
+            xml.Quote do
               xml.Name(quote[:name])
               xml.Rating(quote[:rating])
               xml.Date(quote[:date])
               xml.Text(quote[:text])
-            }
+            end
           end
-        }
-      }
+        end
+      end
     end
   end
 
   def create_xml_file(xml)
-    File.open("output.xml", "w") do |file|
+    File.open('output.xml', 'w') do |file|
       file.write(xml)
     end
   end
