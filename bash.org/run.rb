894fdfd7b3aa3b6337a426015953ecc713ff0452
Roman Vakulchik <roman.vakulchik@gmail.com>
2014-10-30 11:34:46 +0300

    Fix options ARGV

--- bash.org/run.rb
+++ bash.org/run.rb
@@ -2,7 +2,7 @@
 require 'mechanize'
 require_relative 'lib/bash'
 
-count_pages = ARGV[0] ? ARGV[0].to_i : 1
+count_pages = ARGV.empty? ?  1 : ARGV[0].to_i
 site = 'http://bash.im/index/'
 bash = Bash.new(site, count_pages)
 bash.run
